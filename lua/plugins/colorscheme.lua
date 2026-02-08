64f3429eecd287a051c5f9b4f99960ce6a4a1442
Chicão Thiago <fthiagogv@gmail.com>
2026-02-08 08:30:49 -0300

    desabilitando tabs

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,22 @@
 return {
   {
     "rose-pine/neovim",
+    priority = 1000,
     name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        variant = "moon",
+        extend_background_behind_borders = true,
+        enable = {
+          terminal = true,
+          legacy_hightlights = true,
+          migrations = true,
+        },
+        styles = {
+          transparency = true,
+        },
+      })
+    end,
   },
   {
     "LazyVim/LazyVim",
