a205dee945ea2c3833dbf9267e048a334b14cb2c
Chicão Thiago <fthiagogv@gmail.com>
2026-02-09 14:44:51 -0300

    atualizando configurações do tema

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,7 +5,22 @@ return {
     name = "rose-pine",
     config = function()
       require("rose-pine").setup({
-        variant = "main",
+        variant = "auto", -- auto, main, moon, or dawn
+        dark_variant = "moon", -- main, moon, or dawn
+        dim_inactive_windows = false,
+        extend_background_behind_borders = true,
+
+        enable = {
+          terminal = false,
+          legacy_highlights = false, -- Improve compatibility for previous versions of Neovim
+          migrations = false, -- Handle deprecated options automatically
+        },
+
+        styles = {
+          bold = true,
+          italic = true,
+          transparency = false,
+        },
       })
     end,
   },
