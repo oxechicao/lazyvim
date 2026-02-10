1bee9242e101f9f5d0e091439aaae67b2c58f6b2
Chicão Thiago <fthiagogv@gmail.com>
2026-02-10 11:48:03 -0300

    feat: configuracoes e pacotes
    ADD
    - pacote harpoon
    - configurações de link para markdown
    
    CHANGE
    - configurado lint do markdown para ignorar tabelas, cabeçalhos e códigos
    - na regra de 80 characteres
    - removido a ação do botão enter para ceitar sugestões de código pelo
      plugin to blink; <C-y> somente

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,21 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    -- Pequeno delay pode ser necessário se o plugin nativo ainda estiver processando
+    vim.schedule(function()
+      if vim.b.editorconfig then
+        -- Sua lógica personalizada aqui
+        local line_length = vim.b.editorconfig["max_line_length"]
+        if line_length then
+          vim.wo.colorcolumn = tostring(line_length)
+        else
+          vim.wo.colorcolumn = "80"
+        end
+      else
+        vim.wo.colorcolumn = "80"
+      end
+    end)
+  end,
+})
