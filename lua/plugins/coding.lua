d3151daf950451f85b8aa2e2e0c3ea24dc9ec1de
TVieira <thiago_vieira@gap.com>
2026-02-13 18:17:15 -0300

    foi

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -44,4 +44,13 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        markdown = { "markdownlint-cli2" },
+      },
+    },
+  },
 }
